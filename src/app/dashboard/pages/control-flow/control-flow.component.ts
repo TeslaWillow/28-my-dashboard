96d327fbc283d2f40d618ea7636873ac2cc29783
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-12-03 20:40:43 -0600

    control flow.

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+type Grade = 'A'|'B'|'F';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './control-flow.component.html',
-  styles: ``
 })
 export default class ControlFlowComponent {
+
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'Vue', 'Svelte', 'Qwik']);
+  public frameworks2 = signal([]);
+
+  public toggleContent(): void {
+    this.showContent.update( value => !value );
+  }
 
 }
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-title',
+  selector: 'shared-title',
   imports: [],
-  templateUrl: './title.component.html',
-  styleUrl: './title.component.css'
+  template: `
+    <h1 class="text-3xl mb-5">{{ title }} - {{ withShadow }}</h1>
+  `,
 })
 export class TitleComponent {
+
+  @Input({ required: true })
+  public title!: string;
+
+  @Input({
+    transform: booleanAttribute // If exist then is true
+  })
+  public withShadow: boolean = false;
 
 }
