c808562629b46ec470f193c7b9c89faa0820ef90
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-12-03 22:19:10 -0600

    Change detection OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush, // Pendiente en menos ciclos de vida
+  template: `
+    <shared-title [title]="currentFramework()" />
+
+    <pre>{{ frameworkSignal() | json }}</pre>
+    <pre>{{ frameworkAsProperty | json }}</pre>
+  `,
 })
 export default class ChangeDetectionComponent {
 
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworkSignal().name}`
+  );
+
+  public frameworkSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  };
+
+  constructor(){
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+      this.frameworkSignal.update( value => ({
+        ...value,
+        name: 'React',
+      }));
+      console.log('Hecho');
+    }, 3000);
+  }
 }
